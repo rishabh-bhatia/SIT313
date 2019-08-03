fc8139be4ba0f7abb47c7ec98f5f93e94a528992
Rishabh <ojas14@gmail.com>
2019-08-04 04:11:39 +1000

    Lab1
    A Xamarin.forms Android App to change image, each time it is clicked.

---
+++ HelloAndroid/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var imageButton = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            var imageSwitch = false;
+            imageButton.Click += delegate {
+	
+                if(imageSwitch)
+                {
+                    imageButton.SetImageResource(Resource.Drawable.image1);
+                    imageSwitch = false;
+                }
+
+                else
+                {
+                    imageButton.SetImageResource(Resource.Drawable.image2);
+                    imageSwitch = true;
+                }
+	};
+        }
+
+    }
+}
+
---
+++ HelloAndroid/obj/Debug/android/src/md57bb84b1b6def9d8d059b80af1b562686/Activity2.java
@@ -0,0 +1,47 @@
+package md57bb84b1b6def9d8d059b80af1b562686;
+
+
+public class Activity2
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("HelloAndroid.Activity2, HelloAndroid", Activity2.class, __md_methods);
+	}
+
+
+	public Activity2 ()
+	{
+		super ();
+		if (getClass () == Activity2.class)
+			mono.android.TypeManager.Activate ("HelloAndroid.Activity2, HelloAndroid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
