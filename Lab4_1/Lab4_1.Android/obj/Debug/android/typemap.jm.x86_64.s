eaf50cd792cb0f7843993f7d8915ccf878362e26
Rishabh <ojas14@gmail.com>
2019-08-08 00:55:44 +1000

    Lab4.1
    Created a basic cross platform app for iOS and Android using Xamarin.forms. The app consists of 4 editText fields and 2 buttons in a single activity.

---
+++ Lab4_1/Lab4_1.Android/obj/Debug/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: e56e5ac3a9d70a2ece2cfeef73ffce8010a67f3c */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1343
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
---
+++ Lab4_1/Lab4_1/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Lab4_1
+
+{
+
+    [DesignTimeVisible(false)]
+
+    public partial class MainPage : ContentPage
+
+    {
+
+        //path to save information
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Info.txt");
+        public MainPage()
+        {
+
+            InitializeComponent();
+
+        }
+
+        //event listner for Save button
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+
+        {
+
+            StudentInfo thisStudent = new StudentInfo();
+            thisStudent.ID = txtID.Text;
+            thisStudent.marksIn313 = txt313.Text;
+            thisStudent.marksIn314 = txt314.Text;
+            thisStudent.marksIn446 = txt446.Text;
+            File.WriteAllText(_fileName, thisStudent.ToString());
+
+        }
+
+        //event listner for Delete button
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+
+        {
+
+            if (File.Exists(_fileName))//check if the file exists
+            {
+
+                File.Delete(_fileName);
+
+                txtID.Text = "";//clear all fields
+
+                txt313.Text = "";
+
+                txt314.Text = "";
+
+                txt446.Text = "";
+            }
+        }
+    }
+
+}
